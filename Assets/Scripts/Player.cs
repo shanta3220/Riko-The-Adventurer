616e1d8eea7c80b1f0d6416d989c9033ebba7c11
Riko-The-Adventurer <shanta.metro@gmail.cpm>
2018-11-23 05:03:00 +0600

    Player Animations & Face Direction at the target

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,6 +9,10 @@ public class Player : MonoBehaviour {
     private Animator anim;
     private RaycastHit2D hit;
 
+    public Transform target;
+
+    public bool hasTarget;
+    bool isWalking;
 	private void Start () {
 
         boxCollider = GetComponent<BoxCollider2D>();
@@ -18,11 +22,14 @@ public class Player : MonoBehaviour {
     private void FixedUpdate() {
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
+        //isWalking = x != 0 || y != 0 ? true : false;
+        
         //reset moveDelta
         moveDelta = new Vector3(x,y,0);
         if(!isPlayerAdvanced)
             PlayerMovement(moveDelta.x);
-        else PlayerMovement(moveDelta.x, moveDelta.y);
+        else /*PlayerMovement(x,y);*/
+        PlayerFaceEnemyAnim(target);
 
         //making sure we can move the player upward or downward by casting a box there beforehand
         hit = Physics2D.BoxCast(transform.position,boxCollider.size,0,new Vector2(0,moveDelta.y),Mathf.Abs(moveDelta.y*Time.deltaTime),LayerMask.GetMask("Actor","Blocking"));
@@ -37,7 +44,7 @@ public class Player : MonoBehaviour {
             transform.Translate(moveDelta.x * Time.deltaTime,0,0);
         }
     }
-
+    //n3k's
     private void PlayerMovement(float x) {
         //swap sprite direction whether moving left or right and animation
         if (x > 0) {
@@ -53,41 +60,50 @@ public class Player : MonoBehaviour {
 
     }
 
-    private void PlayerMovement(float x,float y) {
+   
+    private void PlayerMovement(float x, float y) {
         //swap sprite direction whether moving left or right and animation
         if (x > 0) {
             transform.localScale = Vector3.one;
-            if (y > 0)
-                //up side
-                anim.SetInteger("Anim", 3);
-            else if(y < 0)
-                anim.SetInteger("Anim", 4);
-            else if(y==0)
-                anim.SetInteger("Anim", 0);
-
         }
-        else if (x == 0) {
-            if (y > 0)
-                //up side
-                anim.SetInteger("Anim", 1);
-            else if (y < 0)
-                anim.SetInteger("Anim", 2);
-            else if (y == 0)
-                anim.SetInteger("Anim", -1);
-        }
-
-        else if (x < 0 ) {
+  
+        else if (x < 0) {
             transform.localScale = new Vector3(-1, 1, 1);
-            if (y > 0)
-                //up side
-                anim.SetInteger("Anim", 3);
-            else if (y < 0)
-                anim.SetInteger("Anim", 4);
-            else if (y == 0)
-                anim.SetInteger("Anim", 0);
+          
         }
-        
-      
+        x = Mathf.Abs(x);
+       // anim.SetBool("IsWalking", isWalking);
+        anim.SetFloat("FaceX", x);
+        anim.SetFloat("FaceY", y);
 
     }
+
+  
+
+    private void PlayerFaceEnemyAnim(Transform target) {
+        float x, y;
+        //Vector2 localMove = transform.InverseTransformDirection(target.position).normalized;
+        Vector2 localMove = (target.position - transform.position).normalized;
+        if (localMove.x > -0.5 && localMove.x < 0.5)
+            x = 0;
+        else x = Mathf.Sign(localMove.x);
+
+        if (localMove.y > -0.5 && localMove.y < 0.5)
+            y = 0;
+        else y = Mathf.Sign(localMove.y);
+
+        if (x > 0) {
+            transform.localScale = Vector3.one;
+        }
+
+        else if (x < 0) {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+       // anim.SetBool("IsWalking", isWalking);
+        x = Mathf.Abs(x);
+        anim.SetFloat("FaceX", x);
+        anim.SetFloat("FaceY", y);
+    }
+
+
 }
